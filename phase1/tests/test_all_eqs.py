2c572a8ac2ce25b12fcf77f64ad0beef5a72860b
EidtZY <52123321+EidtZY@users.noreply.github.com>
2019-08-04 17:59:33 +0800

    ----===

---
+++ phase1/tests/test_all_eqs.py
@@ -0,0 +1,25 @@
+import random
+import sys
+
+sys.path.append('..')
+
+from package_KingOfGlory.class_eq_move import EQMove
+from package_KingOfGlory.class_eq_attack import EQAttack
+from package_KingOfGlory.class_eq_defense import EQDefense
+from package_KingOfGlory.class_eq_mana import EQMana
+print('\n生成一个移动类道具')
+test_eq = EQMove()
+test_eq.show_me()
+test_eq.show_me_unique()
+
+test_eq = EQAttack()
+test_eq.show_me()
+test_eq.show_me_unique()
+
+test_eq = EQDefense()
+test_eq.show_me()
+test_eq.show_me_unique()
+
+test_eq = EQMana()
+test_eq.show_me()
+test_eq.show_me_unique()
